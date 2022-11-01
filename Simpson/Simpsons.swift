d80bb058d2f16981d4b5ef8712e99186912b71bb
ardatoprak <ardattoprak@gmail.com>
2022-11-01 10:47:30 +0300

    Initial commit

---
+++ Simpson/Simpsons.swift
@@ -0,0 +1,25 @@
+//
+//  Simpsons.swift
+//  Simpson
+//
+//  Created by Arda Toprak on 19.10.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+}
